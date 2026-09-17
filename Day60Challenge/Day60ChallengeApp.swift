bf1eb41719026a357e6ecff2a02d931b88757e5c
midkids <83714759+midkids@users.noreply.github.com>
2026-09-16 20:23:48 -0400

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Myron Snelson on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
