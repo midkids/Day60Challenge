889e794fd609087bf066d50c16f485230577f2d9
midkids <83714759+midkids@users.noreply.github.com>
2026-09-19 12:25:23 -0400

    Initial commit

--- Day60Challenge/ContentView.swift
+++ Day60Challenge/ContentView.swift
@@ -7,15 +7,114 @@
 
 import SwiftUI
 
+// Here we will be sending and receiving data
+// from the Internet
+// Combined with Codable support, we will
+// 1) convert Swift objects to JSON and
+//    send over the Internet
+// 2) receive JSON and convert that back
+//    to Swift objects
+// 3) when our request completes, we can
+//    immediately assign that data to
+//    properties in our SwiftUI views
+//    causing them to update the user
+//    inteface immediately
+// To demonstrate, we will load some example
+// music data from Apple's iTunes API
+// We will only use three properties of
+// the many available in the returned JSON
+
+struct User: Codable {
+    var id: UUID
+        var isActive: Bool
+        var name: String
+        var age: Int
+        var company: String
+        var email: String
+        var about: String
+        var registered: Date
+        var tags: [String]
+    var friends: [Friend]
+}
+
+struct Friend: Codable {
+    var id: UUID
+    var name: String
+}
+
+
 struct ContentView: View {
+    @State private var users = [User]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users, id: \.id) { item in
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text("\(item.age)")
+            }
         }
-        .padding()
+        // The task modifier works with asynchronous
+        // functions
+        // await tells SwiftUI a sleep MIGHT happen here
+        
+        .task {
+            await loadData()
+        }
+    }
+    
+    // Networking can be slow compared to local
+    // functions - Async tells Swift to
+    // leave this this code working away in the
+    // background while the main app carries on
+    // working
+    
+    // Use of the keyword async tells Swift
+    // this function might want to go to sleep
+    // so the app can carry on while waiting
+    // for some other work to complete
+    // In this case, this means going to sleep
+    // while our networking code happens
+    // so that our app does not freeze up
+    // when downloading some iTunes API data
+    
+    // The three steps we want to complete:
+    // 1. Create the URL from which we want to
+    //    retrieve data (in this case we want
+    //    to read from Apple servers)
+    // 2. We want to fetch the data from that
+    //    URL using Swift
+    // 3. Decode that result into an array of User values
+    func loadData() async {
+        // Get all songs by Taylor Swift
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do  {
+            // The return value is a tuple
+            // and this tuple will contain
+            // the data we want and also metadata
+            // We don't want that metadata
+            // This statement says: place the returned
+            // actual data in the variable data
+            // and the underscore says to discard
+            // the metadata
+            // IMPORTANT: must use "try await"
+            //  in that order
+            // try: there might be errors here
+            // await" there might be sleeping here
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            users = try decoder.decode([User].self, from: data)
+        } catch {
+            // If our data retrieval above fails for any
+            // reason, we simply print an error message
+            // and do nothing more
+            print("Invalid data: \(error)")
+        }
+    
     }
 }
 
